4192eda3e6c4577b59abfbfe0da24673e53647ce
JohanVanDerKlift <info@johanvanderklift.nl>
2026-01-03 15:06:29 +0100

    implement fixes sugested by codeRabbit

--- components/CandlestickChart.tsx
+++ components/CandlestickChart.tsx
@@ -11,7 +11,6 @@ const CandlestickChart =
     const chartContainerRef = useRef<HTMLDivElement>(null);
     const chartRef = useRef<IChartApi | null>(null);
     const candleSeriesRef = useRef<ISeriesApi<'Candlestick'> | null>(null);
-    const [loading, setLoading] = useState(false);
     const [period, setPeriod] = useState(initialPeriod);
     const [ohlcData, setOhlcData] = useState<OHLCData[]>(data ?? [])
     const [isPending, startTransition] = useTransition();
@@ -53,7 +52,11 @@ const CandlestickChart =
         });
         const series = chart.addSeries(CandlestickSeries, getCandlestickConfig());
 
-        series.setData(convertOHLCData(ohlcData));
+        const convertedToSeconds = ohlcData.map((item) =>
+            [Math.floor(item[0] / 1000), item[1], item[2], item[3], item[4]] as OHLCData
+        )
+
+        series.setData(convertOHLCData(convertedToSeconds));
         chart.timeScale().fitContent();
 
         chartRef.current = chart;
@@ -71,7 +74,7 @@ const CandlestickChart =
             chartRef.current = null;
             candleSeriesRef.current = null;
         };
-    }, [height])
+    }, [height, period])
 
     useEffect(() => {
         if(!candleSeriesRef.current) return;
@@ -96,7 +99,7 @@ const CandlestickChart =
                         <button key={value}
                                 className={period === value ? 'config-button-active' : "config-button"}
                                 onClick={() => handlePeriodChange(value)}
-                                disabled={loading}>
+                                disabled={isPending}>
                             {label}
                         </button>
                     ))}
